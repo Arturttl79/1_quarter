ca5b4d2cea006deea6d1fcdf71249f7c9ec28305
Artur Khaibiev <khaybiev@gmail.com>
2023-01-27 21:05:44 +0100

    Added Program code for the task

--- programm/Program.cs
+++ programm/Program.cs
@@ -1,4 +1,28 @@
 ﻿// Из имеющегося массива строк сформировать массив из строк, длина которых меньше либо равна 3. //
 
-string[] array = { wjdqkdj, 1s, 2sdsq, klio, klo, oiaq, 111, 222, 333 };
+System.Console.Write("Введите массив строк через пробел:");
+string input = Console.ReadLine() ?? "";
+string[] stringArray = input.Split(" ");
+int j = 0;
+int k = 0;
 
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        j++;
+    }
+}
+
+string[] newStringArray = new string[j];
+
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        newStringArray[k] = stringArray[i];
+        k++;
+    }
+}
+
+System.Console.Write("['" + String.Join("', '", stringArray) + "']" + " --> " + "['" + String.Join("', '", newStringArray) + "']");
